f0595208af07dcaaf6fee2e6a98297cdc33b0df6
Valentina-Olekhnovich <volekh@gmail.com>
2020-08-25 20:13:36 +0200

    Debug scss error

---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './scr/client/index.js',
+
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    }
+}
